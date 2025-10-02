1baaaf2ec2f1ebafbfcef2c419e3b6f78dc3dc6a
Vasiliy Bardakov <bvk1502@yandex.ru>
2025-10-03 02:41:23 +0500

    Add new function and update files
    feat(main.py): Add new function print_author
    chore: Update .gitignore, requirements.txt

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 print('Это тестовый проект для обучения и знакомства с Git и терминалом')
 print()
 print('Hello from repository!')
+
+# Импорт load_dotenv.
+from dotenv import load_dotenv
+
+# Импорт библиотеки для работы с окружением.
+import os
+
+load_dotenv()
+
+def print_author():
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}")
+
+print_author()
